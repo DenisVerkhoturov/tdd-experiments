c3fcfcc21e629e4f33e7781f9848d0de2ae031d7
Denis Verkhoturov <mod.satyr@gmail.com>
2017-07-06 21:00:58 +0300

    feat: Range anonymous implementation in factory method

--- src/main/java/com/github/leo_scream/java_lab/tdd/Range.java
+++ src/main/java/com/github/leo_scream/java_lab/tdd/Range.java
@@ -2,6 +2,11 @@ package com.github.leo_scream.java_lab.tdd;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.LongStream;
+
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toList;
 
 /**
  * @author Denis Verkhoturov, mod.satyr@gmail.com
@@ -24,6 +29,63 @@ public interface Range {
     Iterator<Long> asIterator();
 
     static Range of(final long lower, final long upper) {
-        return null;
+        if (lower > upper)
+            throw new IllegalArgumentException("Lower bound of range must be smaller or equal than upper bound");
+        return new Range() {
+            @Override
+            public boolean isBefore(final Range other) {
+                requireNonNull(other);
+                return upperBound() < other.lowerBound();
+            }
+
+            @Override
+            public boolean isAfter(final Range other) {
+                requireNonNull(other);
+                return other.upperBound() < lowerBound();
+            }
+
+            @Override
+            public boolean isConcurrent(final Range other) {
+                requireNonNull(other);
+                return contains(other.lowerBound()) || contains(other.upperBound());
+            }
+
+            @Override
+            public long lowerBound() { return lower; }
+
+            @Override
+            public long upperBound() { return upper; }
+
+            @Override
+            public boolean contains(final long value) { return lowerBound() <= value && value <= upperBound(); }
+
+            @Override
+            public List<Long> asList() {
+                return LongStream.rangeClosed(lowerBound(), upperBound())
+                                 .boxed()
+                                 .collect(toList());
+            }
+
+            @Override
+            public Iterator<Long> asIterator() {
+                return new Iterator<Long>() {
+                    private long current = lowerBound();
+
+                    @Override
+                    public boolean hasNext() { return current <= upperBound(); }
+
+                    @Override
+                    public Long next() {
+                        if (!hasNext()) throw new NoSuchElementException();
+                        return current++;
+                    }
+                };
+            }
+
+            @Override
+            public String toString() {
+                return String.format("Range [%d; %d]", lowerBound(), upperBound());
+            }
+        };
     }
 }
---
+++ src/test/groovy/com/github/leo_scream/java_lab/tdd/RangeSpecification.groovy
@@ -0,0 +1,38 @@
+package com.github.leo_scream.java_lab.tdd
+
+import spock.lang.Specification
+
+/**
+ * @author Denis Verkhoturov, mod.satyr@gmail.com
+ */
+class RangeSpecification extends Specification {
+    void setup() {
+    }
+
+    void cleanup() {
+    }
+
+    def "IsBefore"() {
+    }
+
+    def "IsAfter"() {
+    }
+
+    def "IsConcurrent"() {
+    }
+
+    def "GetLowerBound"() {
+    }
+
+    def "GetUpperBound"() {
+    }
+
+    def "Contains"() {
+    }
+
+    def "AsList"() {
+    }
+
+    def "AsIterator"() {
+    }
+}
