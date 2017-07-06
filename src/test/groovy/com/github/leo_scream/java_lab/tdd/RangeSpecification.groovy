9225b7f1b4019dd89d7a7c28e9825bb9fa82e13c
Denis Verkhoturov <mod.satyr@gmail.com>
2017-07-07 02:25:09 +0300

    test: Spock specification written

--- src/test/groovy/com/github/leo_scream/java_lab/tdd/RangeSpecification.groovy
+++ src/test/groovy/com/github/leo_scream/java_lab/tdd/RangeSpecification.groovy
@@ -6,33 +6,59 @@ import spock.lang.Specification
  * @author Denis Verkhoturov, mod.satyr@gmail.com
  */
 class RangeSpecification extends Specification {
-    void setup() {
+    def "Range lower bound must be less-than or equals to upper bound"() {
+        when:
+        Range.of(47, 42)
+        
+        then:
+        thrown(IllegalArgumentException)
     }
 
-    void cleanup() {
+    def "Range is before another range if it's upper bound is less-than or equals to lower bound of another"() {
+        expect:
+        Range.of(42, 47).isBefore(Range.of(80, 85))
     }
 
-    def "IsBefore"() {
+    def "Range is after another range if it's lower bound is greater-than or equals to upper bound of another"() {
+        expect:
+        Range.of(42, 47).isAfter(Range.of(17, 40))
     }
 
-    def "IsAfter"() {
+    def "Ranges are concurrent if has common values"() {
+        expect:
+        Range.of(42, 47).isConcurrent(Range.of(17, 45))
     }
 
-    def "IsConcurrent"() {
+    def "Values in bounds belongs to a range"() {
+        expect:
+        Range.of(42, 47).contains(value)
+        
+        where:
+        value << [42, 43, 44, 45, 46, 47]
     }
 
-    def "GetLowerBound"() {
+    def "Values out of bounds do not belongs to a range"() {
+        expect:
+        !Range.of(42, 47).contains(value)
+
+        where:
+        value << [40, 41, 48, 49]
     }
 
-    def "GetUpperBound"() {
+    def "Range as list contains all the values"() {
+        expect:
+        Range.of(42, 47).asList() == [42, 43, 44, 45, 46, 47] as List<Long>
     }
 
-    def "Contains"() {
-    }
+    def "Range iterator throws NoSuchElementException if has not next value"() {
+        given:
+        final Iterator<Long> iterator = Range.of(42, 47).asIterator()
+        while (iterator.hasNext()) iterator.next()
 
-    def "AsList"() {
-    }
+        when:
+        iterator.next()
 
-    def "AsIterator"() {
+        then:
+        thrown(NoSuchElementException)
     }
 }
