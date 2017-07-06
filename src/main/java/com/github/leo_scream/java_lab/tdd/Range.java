3eefae504355396d77a0c51f1e317c9c2a51f211
Denis Verkhoturov <mod.satyr@gmail.com>
2017-07-06 21:13:39 +0300

    refactor: Range anonymous implementation methods goes to default

--- src/main/java/com/github/leo_scream/java_lab/tdd/Range.java
+++ src/main/java/com/github/leo_scream/java_lab/tdd/Range.java
@@ -12,75 +12,60 @@ import static java.util.stream.Collectors.toList;
  * @author Denis Verkhoturov, mod.satyr@gmail.com
  */
 public interface Range {
-    boolean isBefore(final Range other);
-
-    boolean isAfter(final Range other);
-
-    boolean isConcurrent(final Range other);
 
     long lowerBound();
 
     long upperBound();
 
-    boolean contains(final long value);
+    default boolean isBefore(final Range other) {
+        requireNonNull(other);
+        return upperBound() < other.lowerBound();
+    }
 
-    List<Long> asList();
+    default boolean isAfter(final Range other) {
+        requireNonNull(other);
+        return other.upperBound() < lowerBound();
+    }
 
-    Iterator<Long> asIterator();
+    default boolean isConcurrent(final Range other) {
+        requireNonNull(other);
+        return contains(other.lowerBound()) || contains(other.upperBound());
+    }
+
+    default boolean contains(final long value) {
+        return lowerBound() <= value && value <= upperBound();
+    }
+
+    default List<Long> asList() {
+        return LongStream.rangeClosed(lowerBound(), upperBound())
+                         .boxed()
+                         .collect(toList());
+    }
+
+    default Iterator<Long> asIterator() {
+        return new Iterator<Long>() {
+            private long current = lowerBound();
+
+            @Override
+            public boolean hasNext() { return current <= upperBound(); }
+
+            @Override
+            public Long next() {
+                if (!hasNext()) throw new NoSuchElementException();
+                return current++;
+            }
+        };
+    }
 
     static Range of(final long lower, final long upper) {
         if (lower > upper)
-            throw new IllegalArgumentException("Lower bound of range must be smaller or equal than upper bound");
+            throw new IllegalArgumentException("Lower bound of range must be less-than or equal to upper bound");
         return new Range() {
-            @Override
-            public boolean isBefore(final Range other) {
-                requireNonNull(other);
-                return upperBound() < other.lowerBound();
-            }
-
-            @Override
-            public boolean isAfter(final Range other) {
-                requireNonNull(other);
-                return other.upperBound() < lowerBound();
-            }
-
-            @Override
-            public boolean isConcurrent(final Range other) {
-                requireNonNull(other);
-                return contains(other.lowerBound()) || contains(other.upperBound());
-            }
-
             @Override
             public long lowerBound() { return lower; }
 
             @Override
             public long upperBound() { return upper; }
-
-            @Override
-            public boolean contains(final long value) { return lowerBound() <= value && value <= upperBound(); }
-
-            @Override
-            public List<Long> asList() {
-                return LongStream.rangeClosed(lowerBound(), upperBound())
-                                 .boxed()
-                                 .collect(toList());
-            }
-
-            @Override
-            public Iterator<Long> asIterator() {
-                return new Iterator<Long>() {
-                    private long current = lowerBound();
-
-                    @Override
-                    public boolean hasNext() { return current <= upperBound(); }
-
-                    @Override
-                    public Long next() {
-                        if (!hasNext()) throw new NoSuchElementException();
-                        return current++;
-                    }
-                };
-            }
 
             @Override
             public String toString() {
